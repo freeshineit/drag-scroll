f93cb478d4e49b9a625f2eea4d8baf07a3941811
ShineShao <xiaoshaoqq@gmail.com>
2025-11-16 01:28:15 +0800

    demo: update deps

---
+++ examples/with-vue2.5/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <DragScroll />
+  </div>
+</template>
+
+<script>
+import DragScroll from './components';
+export default {
+  name: 'app',
+  components: {
+    DragScroll,
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 150vh;
+}
+</style>
