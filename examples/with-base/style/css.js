4a7c9902b96d78464f25a647268534c5484ad2dd
ShineShao <xiaoshaoqq@gmail.com>
2026-02-26 00:57:32 +0800

    demo: update

---
+++ examples/with-base/style/css.js
@@ -0,0 +1,12 @@
+/**
+ * drag scroll support move and touch
+ *
+ * @skax/drag-scroll v2.0.0
+ * Copyright (c) 2026-02-25 ShineShao <xiaoshaoqq@gmail.com>
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+'use strict';
+
+require("./css.css");
