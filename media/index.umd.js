8fe6007f7c0265c509cc7f8b7bca8f4bfadd02cd
freeshineit <xiaoshaoqq@gmail.com>
2026-03-01 16:10:46 +0000

    Deploying to gh-pages from @ freeshineit/drag-scroll@d5423b49c6b996022787bcde2ea16df231df97c4 🚀

--- media/index.umd.js
+++ media/index.umd.js
@@ -2,7 +2,7 @@
  * drag scroll support move and touch
  *
  * @skax/drag-scroll v2.0.0
- * Copyright (c) 2026-02-26 ShineShao <xiaoshaoqq@gmail.com>
+ * Copyright (c) 2026-03-01 ShineShao <xiaoshaoqq@gmail.com>
  * 
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
